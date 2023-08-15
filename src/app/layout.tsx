3dcf13c962a9f2611a84e29316c9829a6c7d878c
Tejas Chaudhari <tejasc888@gmail.com>
2023-08-15 23:50:41 +0530

    test

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import Navbar from '@/components/Navbar'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Tejas Chaudhari',
+  description: 'Portfolio',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full selection:bg-gray-50`}>
+        cidvusio
+
+vdjioivjewk
+
+
+        <Navbar />
+        {children}
+        </body>
+    </html>
+  )
+}
