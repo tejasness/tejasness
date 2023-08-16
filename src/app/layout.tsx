e209202dc78519474a9cc25085e263f5274efd39
Tejas Chaudhari <tejasc888@gmail.com>
2023-08-16 08:09:46 +0530

    saving changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,17 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full selection:bg-gray-50`}>
-        cidvusio
-
-
-ivni 3rio vsi
-g4igjb
-go4
-evo0s
-vso0o3
-kor3r
-ks3kr3t2
-
         <Navbar />
         {children}
         </body>
