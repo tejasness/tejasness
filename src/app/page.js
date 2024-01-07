6a9f02a89dadbef69cf56170a20dc8a72e706aeb
Tejas Chaudhari <tejasc888@gmail.com>
2024-01-07 14:53:10 +0530

    removed default css

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+   <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7144919048433115136" height="602" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+   <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7144201607352565760" height="686" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+   <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7134521023558811648" height="917" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+    </main>
+  )
+}
